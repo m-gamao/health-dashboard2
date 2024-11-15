4ad5f156b86969821977c075a0965c35e72d3af5
m-gamao <42097750+m-gamao@users.noreply.github.com>
2024-11-15 13:16:53 -0500

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import PageBackdrop from './components/PageBackdrop';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Layout from './components/Layout';
 // import Dashboard from './components/Dashboard';
 // import Appointments from './Appointments';
 
 
-
-// Uncomment the components below as I add them to the code
-
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Sidebar />
-        <PageBackdrop />
-           {/* <Switch> */}
-            {/* <Route path="/dashboard"> */}
-              {/* <Dashboard /> */}
-            {/* </Route> */}
-            {/* <Route path="/appointments"> */}
-              {/* <Appointments /> */}
-            {/* </Route> */}
-            {/* Add more routes here for other components */}
-          {/* </Switch> */}
-        {/* </PageBackdrop> */}
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          {/* <Route path="/appointments" element={<Appointments />} /> */}
+          {/* <Route path="/medications" element={<Medications />} /> */}
+          {/* Add other routes as needed */}
+        </Routes>
+      </Layout>
     </Router>
   );
 }
