19c41cce1bf650f4708999ceb4868692173ede3e
m-gamao <42097750+m-gamao@users.noreply.github.com>
2024-11-19 09:40:14 -0500

    medication reminders section/header

--- src/components/MedicationReminders.js
+++ src/components/MedicationReminders.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './MedicationReminders.css';
+import medHeaderImage from '../assets/med_reminder_header.png';
 
 const MedicationReminders = () => {
   return (
     <div className="medication-reminders">
       <div className="medication-header">
-        <img src="/public/med reminder header.png" alt="Medication Reminders Header" />
+        {/* <img src="/public/med_reminder_header.png" alt="Medication Reminders Header" /> */}
+        <img src={medHeaderImage} alt="Medications Reminders Header" />
       </div>
       <div className="medication-content">
         <p>You have no new reminders for today!</p>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import GreetingBar from '../components/GreetingBar';
+import MedicationReminders from '../components/MedicationReminders';
 
 const Dashboard = () => {
     return (
       <div className="dashboard">
         <GreetingBar />
-        {/* Other components will go here */}
+        <MedicationReminders />
       </div>
     );
   };
